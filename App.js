11cffe6fa7c3f8c3f545c0f525c3f604e47ccb7d
Brice Tessier Huort <tessierhuort@gmail.com>
2018-03-09 15:51:15 +0100

    remove import react from app.js because not necessary

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'
 import { StackNavigator } from 'react-navigation'
 import { Login } from './src/views/Login'
 import { Activities } from './src/views/Activities'
@@ -7,7 +6,7 @@ import { Planning } from './src/views/Planning'
 console.ignoredYellowBox = ['Warning: componentWill', 'Remote debugger']
 
 export const App = StackNavigator({
-  Main: { screen: Login },
-  Activities: { screen: Activities },
+  Main: { screen: Activities },
+  Login: { screen: Login },
   Planning: { screen: Planning }
 })
