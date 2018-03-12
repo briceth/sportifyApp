5875a694fe2283449c02ed4cfc2cb2cce209861b
Brice Tessier Huort <tessierhuort@gmail.com>
2018-03-12 17:04:03 +0100

    working on flash alert

--- src/components/FlashAlert.js
+++ src/components/FlashAlert.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Animated, StyleSheet, View, TouchableOpacity } from 'react-native'
+import { Animated, StyleSheet, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 import { MyText } from './MyText'
 import { DARKBLUE } from '../mainStyle'
