11d3809a7acdb28b72ea1f404edd637289c14384
Kévin <kevin@MacBook-Pro-de-Kevin.local>
2018-03-12 12:33:38 +0100

    solving conflits

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import { StyleSheet, TextInput } from 'react-native'
 import { LINEGREY } from '../mainStyle'
-<<<<<<< HEAD
 import { BLACK } from '../mainStyle'
-=======
 import PropTypes from 'prop-types'
->>>>>>> 3f5d3acad69a86f3fcbfe261ad9ba0446384280b
 
 export class Input extends Component {
   static propTypes = {
@@ -36,13 +33,9 @@ export class Input extends Component {
         autoCapitalize={noCapitalize && 'none'}
         secureTextEntry={secureTextEntry}
         style={[styles.input, border]}
-<<<<<<< HEAD
-        placeholder={this.props.placeholder}
-=======
         placeholder={placeholder}
         value={text}
         onChangeText={text => handleInputs(text, data)}
->>>>>>> 3f5d3acad69a86f3fcbfe261ad9ba0446384280b
       />
     )
   }
