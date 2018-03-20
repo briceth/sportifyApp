6f075c9ab09b3a89bc95b0c74afffaf48481231f
Jerome Burkard <jerome.burkard@gadz.org>
2018-03-20 17:49:20 +0100

    go to signup when reservation without currentUser

--- src/views/Planning.js
+++ src/views/Planning.js
@@ -121,8 +121,8 @@ export class Planning extends Component {
 
   bookSession = async () => {
     const currentUser = await store.get('currentUser')
+    if (!currentUser) return this.props.navigation.navigate('Signup')
     currentUser.account.sessions.push(this.state.session)
-    // need user id, session id
     store.update('currentUser', currentUser).then(res => {
       this.props.navigation.navigate('Home', {
         newCurrentUser: currentUser
