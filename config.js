04575b467532696e6d36c7141c2366003af0ebb6
Jerome Burkard <jerome.burkard@gadz.org>
2018-03-28 21:23:33 -0300

    Cleaning repo

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 let env = process.env.NODE_ENV || 'development'
-<<<<<<< HEAD
 env = 'production'
-=======
-// env = 'production'
->>>>>>> 4851aeb88e82fb009484090c9e7712228b5e52d3
 
 const config = {
   ENV: env,
